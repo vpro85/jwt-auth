e72a6af90115f8b4a2590b490e19e470e23bb43e
Vadim Prokoshin <vpro85@gmail.com>
2021-10-05 20:42:54 +0200

    Add mail service for sending activation link to users email

--- server/service/mail-service.js
+++ server/service/mail-service.js
@@ -1,6 +1,34 @@
-class MailService {
-    async sendActivationMail(to, link) {
+const nodemailer = require('nodemailer')
 
+class MailService {
+
+    constructor() {
+        this.transporter = nodemailer.createTransport({
+            host: process.env.SMTP_HOST,
+            port: process.env.SMTP_PORT,
+            secure: false,
+            auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASSWORD,
+            }
+
+        })
+    }
+
+    async sendActivationMail(to, link) {
+        await this.transporter.sendMail({
+            from: process.env.SMTP_USER,
+            to: to,
+            subject: 'Account activation at ' + process.env.API_URL ,
+            text: '',
+            html:
+                `
+                <div>
+                    <h1>For activation follow link:</h1>
+                    <a href="${link}">${link}</a>
+                </div>
+                `,
+        })
     }
 }
 
--- server/service/user-service.js
+++ server/service/user-service.js
@@ -16,7 +16,7 @@ class UserService {
         const activationLink = uuid.v4();
 
         const user = await UserModel.create({email, password: hashPassword, activationLink})
-        await mailService.sendActivationMail(email, activationLink)
+        await mailService.sendActivationMail(email, `${process.env.API_URL}/api/activate/${activationLink}`)
 
         const userDto = new UserDto(user)   //id, email, isActivated
         const tokens = tokenService.generateTokens({...userDto})
